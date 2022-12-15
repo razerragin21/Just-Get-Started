1c008bd9156c020bb31b855c4ced3c39708860c8
razerragin21 <razerragin@gmail.com>
2022-12-15 10:14:15 +0000

    Radio button fixes

--- testui.py
+++ testui.py
@@ -71,10 +71,10 @@ class App(customtkinter.CTk):
         self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="Priority Group:", text_color="#1F6AB8", font=("default", 14, "bold"))
         self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
 
-        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0, text="Low priority", text_color="#6FCA3F")
+        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0, border_color='white', text="Low priority", text_color="#6FCA3F")
         self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
 
-        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1, text="Medium priority", text_color="#FFB620")
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1, border_color='white', text="Medium priority", text_color="#FFB620")
         self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
 
         self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2, border_color='white', text="High priority", text_color="#DC463F")
