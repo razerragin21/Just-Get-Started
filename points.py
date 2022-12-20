751da44a558601468101060040b9a5d681c04366
razerragin21 <razerragin@gmail.com>
2022-12-20 11:31:06 +0000

    BST implementation

--- points.py
+++ points.py
@@ -5,3 +5,28 @@ def add_points():
 
 add_points()
 
+def insert(self, val):
+    if not self.val:
+        self.val = val
+        return
+
+    if self.val == val:
+        return
+
+    if val < self.val:
+        if self.left:
+            self.left.insert(val)
+            return
+        self.left = BSTNode(val)
+        return
+
+    if self.right:
+        self.right.insert(val)
+        return
+    self.right = BSTNode(val)
+
+
+#feynman 
+#1. pick a topic
+#2. explain to 12 year old
+#3. review, simplify, repeat
